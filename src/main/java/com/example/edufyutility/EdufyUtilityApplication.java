da3d44c0928084e55b780a9f2a2f0aa6c425c0d8
Alex <alexander.westerberg.svedberg@gmail.com>
2025-11-04 12:23:44 +0100

    first commit

---
+++ src/main/java/com/example/edufyutility/EdufyUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.edufyutility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyUtilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdufyUtilityApplication.class, args);
+    }
+
+}
